878377fdd1bbb4657b236d0ce61cd5faa85031cb
Lukas Forst <lukas@forst.pw>
2020-03-06 20:32:09 +0100

    use typed upsert for postgresql

--- src/main/kotlin/com/wire/bots/polls/dao/Extensions.kt
+++ src/main/kotlin/com/wire/bots/polls/dao/Extensions.kt
@@ -1,14 +1,32 @@
 package com.wire.bots.polls.dao
 
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.statements.InsertStatement
 import org.jetbrains.exposed.sql.transactions.TransactionManager
-import java.sql.ResultSet
 
-fun <T : Any> String.execAndMap(transform: (ResultSet) -> T): List<T> {
-    val result = arrayListOf<T>()
-    TransactionManager.current().exec(this) { rs ->
-        while (rs.next()) {
-            result += transform(rs)
-        }
+/**
+ * Inserts or updates on conflict with [keys].
+ */
+fun <T : Table> T.insertOrUpdate(vararg keys: Column<*>, body: T.(InsertStatement<Number>) -> Unit) =
+    InsertOrUpdate<Number>(this, keys = *keys).apply {
+        body(this)
+        execute(TransactionManager.current())
     }
-    return result
+
+/**
+ * Update statement - works only on PosgreSQL - https://github.com/JetBrains/Exposed/issues/167#issuecomment-480199613
+ */
+class InsertOrUpdate<Key : Any>(
+    table: Table,
+    isIgnore: Boolean = false,
+    private vararg val keys: Column<*>
+) : InsertStatement<Key>(table, isIgnore) {
+    override fun prepareSQL(transaction: Transaction): String {
+        val tm = TransactionManager.current()
+        val updateSetter = table.columns.joinToString { "${tm.identity(it)} = EXCLUDED.${tm.identity(it)}" }
+        val onConflict = "ON CONFLICT (${keys.joinToString { tm.identity(it) }}) DO UPDATE SET $updateSetter"
+        return "${super.prepareSQL(transaction)} $onConflict"
+    }
 }
--- src/main/kotlin/com/wire/bots/polls/dao/PollRepository.kt
+++ src/main/kotlin/com/wire/bots/polls/dao/PollRepository.kt
@@ -1,15 +1,14 @@
 package com.wire.bots.polls.dao
 
 import ai.blindspot.ktoolz.extensions.mapToSet
-import ai.blindspot.ktoolz.extensions.whenNull
 import com.wire.bots.polls.dto.PollAction
 import com.wire.bots.polls.dto.PollDto
 import mu.KLogging
+import org.jetbrains.exposed.sql.JoinType
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
-import org.jetbrains.exposed.sql.update
 import java.util.UUID
 
 /**
@@ -44,23 +43,10 @@ class PollRepository {
 
     /**
      * Register new vote to the poll. If the poll with provided pollId does not exist,
-     * [IllegalArgumentException] is thrown.
+     * database contains foreign key to an option and poll so the SQL exception is thrown.
      */
     suspend fun vote(pollAction: PollAction) = newSuspendedTransaction {
-        PollOptions.select {
-            (PollOptions.pollId eq pollAction.pollId) and (PollOptions.optionOrder eq pollAction.optionId)
-        }.singleOrNull()
-            .whenNull {
-                logger.error { "There is no poll which has id ${pollAction.pollId} or no option with order ${pollAction.optionId}" }
-            } ?: throw IllegalArgumentException("No poll or option found for the received vote!")
-
-        // unfortunately Exposed does not support upsert,
-        // so try to update the vote and if nothing is updated, insert new value
-        Votes.update(
-            { (Votes.pollId eq pollAction.pollId) and (Votes.userId eq pollAction.userId) }
-        ) {
-            it[pollOption] = pollAction.optionId
-        }.takeUnless { it == 0 } ?: Votes.insert {
+        Votes.insertOrUpdate(Votes.pollId, Votes.userId) {
             it[pollId] = pollAction.pollId
             it[pollOption] = pollAction.optionId
             it[userId] = pollAction.userId
@@ -73,8 +59,11 @@ class PollRepository {
      * Offset/option id/button as keys and user ids as values.
      */
     suspend fun stats(pollId: String) = newSuspendedTransaction {
-        (PollOptions leftJoin Votes)
-            .slice(PollOptions.optionOrder, Votes.userId)
+        PollOptions.join(Votes, JoinType.LEFT,
+            additionalConstraint = {
+                (Votes.pollId eq PollOptions.pollId) and (Votes.pollOption eq PollOptions.optionOrder)
+            }
+        ).slice(PollOptions.optionOrder, Votes.userId)
             .select {
                 PollOptions.pollId eq pollId
             }
@@ -88,10 +77,10 @@ class PollRepository {
      * Returns set of user ids that voted in the poll with given pollId.
      */
     suspend fun votingUsers(pollId: String) = newSuspendedTransaction {
-        (PollOptions rightJoin Votes)
+        Votes
             .slice(Votes.userId)
             .select {
-                PollOptions.pollId eq pollId
+                Votes.pollId eq pollId
             }
             .mapToSet { it[Votes.userId] }
     }
