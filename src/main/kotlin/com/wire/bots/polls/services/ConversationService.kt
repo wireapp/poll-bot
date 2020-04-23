ca92cb7410210b7083c2e19273118b32a087d559
Lukas Forst <lukas@forst.pw>
2020-04-23 15:05:55 +0200

    redesign the pipelines to match new branch strategy

--- src/main/kotlin/com/wire/bots/polls/services/ConversationService.kt
+++ src/main/kotlin/com/wire/bots/polls/services/ConversationService.kt
@@ -1,13 +1,11 @@
 package com.wire.bots.polls.services
 
-import ai.blindspot.ktoolz.extensions.parseJson
 import com.wire.bots.polls.dto.roman.ConversationInformation
+import com.wire.bots.polls.utils.appendPath
 import io.ktor.client.HttpClient
-import io.ktor.client.call.receive
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.url
-import io.ktor.client.statement.HttpStatement
 import mu.KLogging
 
 /**
@@ -19,27 +17,21 @@ class ConversationService(private val client: HttpClient, config: ProxyConfigura
         const val conversationPath = "/conversation"
     }
 
-    private val endpoint = config.baseUrl + conversationPath
+    private val endpoint = config.baseUrl appendPath conversationPath
 
     /**
      * Returns the number of members of conversation.
      */
     suspend fun getNumberOfConversationMembers(token: String): Int? =
         runCatching {
-            val response = client.get<HttpStatement> {
+            client.get<ConversationInformation> {
                 url(endpoint)
                 header("Authorization", "Bearer $token")
-            }.execute()
-            logger.trace { "Executed" }
-
-            val payload = response.receive<String>()
-            logger.trace { payload }
-            parseJson<ConversationInformation>(payload)
+            }
         }.onFailure {
             logger.error(it) { "It was not possible to fetch conversation information!" }
         }.onSuccess {
             logger.debug { "Successfully got conversation information." }
-            logger.trace { it }
         }.getOrNull()
             ?.members
             ?.filter { it.service == null }
