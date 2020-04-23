f3aa5f8e2814b92f9f79fbcccb2e2b89429c3758
Lukas Forst <lukas@forst.pw>
2020-04-23 15:56:32 +0200

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,6 @@ dependencies {
     implementation("io.ktor", "ktor-client-json", ktorVersion)
     implementation("io.ktor", "ktor-client-jackson", ktorVersion)
     implementation("io.ktor", "ktor-client-apache", ktorVersion)
-    implementation("io.ktor", "ktor-client-okhttp", ktorVersion)
     implementation("io.ktor", "ktor-client-logging-jvm", ktorVersion)
 
     // Prometheus metrics
